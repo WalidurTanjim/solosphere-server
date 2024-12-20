11c99f2b746f38da8c5ae56dba69437df081a7c2
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-12-21 02:11:57 +0600

    Job get by id, get all, post, delete apis created successfully

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 5000;
 const app = express()
 
+// middlewares
 app.use(cors())
 app.use(express.json())
 
@@ -22,16 +23,49 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const db = client.db('SoloSphere');
+    const jobsCollection = db.collection('jobs');
+
+
+    // jobs related APIs
+    app.get('/add-jobs/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.get('/add-jobs', async(req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/add-jobs', async(req, res) => {
+      const job = req.body;
+      const result = await jobsCollection.insertOne(job);
+      res.send(result);
+    })
+
+    app.delete('/add-jobs/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error
   }
 }
 run().catch(console.dir)
+
 app.get('/', (req, res) => {
   res.send('Hello from SoloSphere Server....')
 })
